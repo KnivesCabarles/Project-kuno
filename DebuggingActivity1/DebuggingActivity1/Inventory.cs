ec3a096e74d0580b493225c0774dcf40046220a8
Wanna be Programmer <167628194+KnivesCabarles@users.noreply.github.com>
2025-03-23 22:18:45 +0800

    Add files via upload

---
+++ DebuggingActivity1/DebuggingActivity1/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebuggingActivity1
+{
+    class Inventory
+    {
+        public static List<string> products = new List<string>();
+
+        public static void InsertProduct(string productName)
+        {
+            products.Add(productName);
+            Console.WriteLine($"'{productName}' has been added to the inventory.");
+        }
+
+        public static void DisplayProducts()
+        {
+            if (products.Count == 0)
+            {
+                Console.WriteLine("The inventory is empty.");
+                return;
+            }
+
+            Console.WriteLine("\nProducts in Inventory:");
+            foreach (string product in products)
+            {
+                Console.WriteLine($"- {product}");
+            }
+        }
+    }
+}
---
+++ DebuggingActivity1/DebuggingActivity1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DebuggingActivity1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Write("\nEnter Product Here (or type 'exit' to quit): ");
+                string product = Console.ReadLine();
+
+                if (product.ToLower() == "exit")
+                {
+                    Console.WriteLine("Exiting program...");
+                    break;
+                }
+
+                Inventory.InsertProduct(product);
+                Inventory.DisplayProducts();
+            }
+        }
+    }
+}
